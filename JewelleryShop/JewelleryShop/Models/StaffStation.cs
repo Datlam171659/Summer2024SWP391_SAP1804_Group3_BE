45bacfc63dd60c3018b9e293d44295ca18139817
timboace0 <khanglgse171640@fpt.edu.vn>
2024-05-23 08:45:46 +0700

    setup commit

---
+++ JewelleryShop/JewelleryShop/Models/StaffStation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JewelleryShop.API.Models
+{
+    public partial class StaffStation
+    {
+        public string? StationId { get; set; }
+        public string? StaffId { get; set; }
+    }
+}
