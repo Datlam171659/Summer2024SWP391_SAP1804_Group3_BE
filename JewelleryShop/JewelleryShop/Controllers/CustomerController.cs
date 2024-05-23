d51d6307fe54a30f967a441cc4304f574009f4ad
GiaKhang1 <khanglgse171640@fpt.edu.vn>
2024-05-23 22:51:14 +0700

    GET(id)/PUT Customer API

--- JewelleryShop/JewelleryShop/Controllers/CustomerController.cs
+++ JewelleryShop/JewelleryShop/Controllers/CustomerController.cs
@@ -23,13 +23,58 @@ namespace JewelleryShop.API.Controllers
             return await _context.Customers.ToListAsync();
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomersById(int id)
+        {
+            var CustomerById = await _context.Customers.FindAsync(id);
+            if (CustomerById == null)
+            {
+                return NotFound();
+            }
+            return Ok(CustomerById);
+        }
+
+
         [HttpPost]
         public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
         {
+            try { 
             _context.Customers.Add(customer);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetCustomers", new { id = customer.Id }, customer);
+            } catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCustomerById(int id, [FromBody] Customer newCustomerData)
+        {
+            if (newCustomerData == null || newCustomerData.Id != id)
+                return BadRequest();
+
+            var existingCustomer = await _context.Customers.FindAsync(id);
+            
+            if (existingCustomer == null)
+                return NotFound();
+
+            existingCustomer.CustomerName = newCustomerData.CustomerName;
+            existingCustomer.Address = newCustomerData.Address;
+            existingCustomer.Gender = newCustomerData.Gender;
+            existingCustomer.PhoneNumber = newCustomerData.PhoneNumber;
+            existingCustomer.Email = newCustomerData.Email;
+            existingCustomer.Status = newCustomerData.Status;
+
+            _context.Customers.Update(existingCustomer);
+            try { 
+            await _context.SaveChangesAsync();
+            } catch (DbUpdateConcurrencyException ex)
+            {
+                Console.WriteLine("A concurrency error occurred while saving changes: " + ex.Message);
+            }
+
+            return NoContent(); // Success
         }
     }
 }
