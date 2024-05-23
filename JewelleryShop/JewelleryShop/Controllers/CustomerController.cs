86541f71bda104e898cf92e58d28d82cc0a28f30
GiaKhang1 <khanglgse171640@fpt.edu.vn>
2024-05-23 14:38:11 +0700

    GET(all)/POST Customer API

---
+++ JewelleryShop/JewelleryShop/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JewelleryShop.API.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace JewelleryShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly grumbly_PWSContext _context;
+
+        public CustomerController(grumbly_PWSContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCustomers", new { id = customer.Id }, customer);
+        }
+    }
+}
