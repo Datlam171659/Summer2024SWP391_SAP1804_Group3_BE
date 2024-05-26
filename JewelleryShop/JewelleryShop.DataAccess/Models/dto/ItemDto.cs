5435c7186c4776796cf6c45dc33a097bc490887c
NguyenDucChi <nguyenducchi07102003@gmail.com>
2024-05-26 11:09:39 +0700

    Create, Read Item

---
+++ JewelleryShop/JewelleryShop.DataAccess/Models/dto/ItemDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelleryShop.DataAccess.Models.dto
+{
+    public class ItemDto
+    {   
+        public string ItemId { get; set; } = null!;
+        public string? ItemImagesId { get; set; }
+        public string? BrandId { get; set; }
+        public string? AccessoryType { get; set; }
+        public string? Sku { get; set; }
+        public string? ItemName { get; set; }
+        public string? Description { get; set; }
+        public int? Price { get; set; }
+        public string? Size { get; set; }
+        public string? Weight { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string? Status { get; set; }
+    }
+}
---
+++ JewelleryShop/JewelleryShop/Controllers/ItemController.cs
@@ -0,0 +1,65 @@
+﻿using JewelleryShop.DataAccess.Models;
+using JewelleryShop.DataAccess.Models.dto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace JewelleryShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private readonly JewelleryDBContext _context;
+
+        public ItemController(JewelleryDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/item
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Item>>> GetItems()
+        {
+            return await _context.Items.ToListAsync();
+        }
+
+        // GET: api/item/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Item>> GetItem(string id)
+        {
+            var itemId = await _context.Items.FindAsync(id);
+
+            if (itemId == null)
+            {
+                return NotFound();
+            }
+
+            return itemId;
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateItem(ItemDto request) {
+            var item = new Item()
+            {
+                ItemId = request.ItemId,
+                ItemImagesId = request.ItemImagesId,
+                ItemName = request.ItemName,
+                BrandId = request.BrandId,
+                AccessoryType = request.AccessoryType,
+                CreatedDate = request.CreatedDate,
+                Description = request.Description,
+                Price = request.Price,
+                Size = request.Size,
+                Sku = request.Sku,
+                UpdatedDate = request.UpdatedDate,
+                Status = request.Status,
+                Weight = request.Weight,
+            };
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
