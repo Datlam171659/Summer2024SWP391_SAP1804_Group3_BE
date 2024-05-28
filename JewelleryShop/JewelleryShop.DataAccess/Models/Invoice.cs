d5ecc2db548196fe5d60d59af2dae9f55bbd4718
GiaKhang1 <khanglgse171640@fpt.edu.vn>
2024-05-28 08:13:17 +0700

    GET SalesAPI

---
+++ JewelleryShop/JewelleryShop.DataAccess/Models/Invoice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JewelleryShop.DataAccess.Models
+{
+    public partial class Invoice
+    {
+        public string Id { get; set; } = null!;
+        public string? StaffId { get; set; }
+        public string? ReturnPolicyId { get; set; }
+        public string? ItemId { get; set; }
+        public string? CustomerId { get; set; }
+        public string? CompanyName { get; set; }
+        public string? BuyerAddress { get; set; }
+        public string? Status { get; set; }
+        public string? PaymentType { get; set; }
+        public int? Quantity { get; set; }
+        public decimal? SubTotal { get; set; }
+
+        public virtual Customer? Customer { get; set; }
+        public virtual staff? Staff { get; set; }
+    }
+}
