a48dd3b97ed792f2830f4b11151d1b1f268e62fa
GiaKhang1 <khanglgse171640@fpt.edu.vn>
2024-05-28 08:13:17 +0700

    GET SalesAPI

---
+++ JewelleryShop/JewelleryShop.DataAccess/Models/ItemInvoice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JewelleryShop.DataAccess.Models
+{
+    public partial class ItemInvoice
+    {
+        public string? ItemId { get; set; }
+        public string? InvoiceId { get; set; }
+        public string? ReturnPolicyId { get; set; }
+        public string? WarrantyId { get; set; }
+
+        public virtual Invoice? Invoice { get; set; }
+        public virtual Item? Item { get; set; }
+        public virtual Warranty? Warranty { get; set; }
+    }
+}
---
+++ JewelleryShop/JewelleryShop/Controllers/SaleController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JewelleryShop.DataAccess.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace JewelleryShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalesController : ControllerBase 
+    {
+        private readonly JewelleryDBContext _context;
+
+        public SalesController(JewelleryDBContext context)
+        {
+            _context = context;
+        }
+        // Invoice APIs
+        [HttpGet("Invoices")]
+        public async Task<ActionResult<IEnumerable<Invoice>>> GetInvoice()
+        {
+            return await _context.Invoices.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Invoice>>> GetInvoiceById(string id)
+        {
+            var InvoiceById = await _context.Invoices.FindAsync(id);
+            if (InvoiceById == null)
+            {
+                return NotFound();
+            }
+            return Ok(InvoiceById);
+        }
+        [HttpGet("Warranty")]
+        public async Task<ActionResult<IEnumerable<Warranty>>> GetWarranty()
+        {
+            return await _context.Warranties.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Invoice>>> GetWarrantyById(string id)
+        {
+            var WarrantyById = await _context.Warranties.FindAsync(id);
+            if (WarrantyById == null)
+            {
+                return NotFound();
+            }
+            return Ok(WarrantyById);
+        }
+    }
+}
