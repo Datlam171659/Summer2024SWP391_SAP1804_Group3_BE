4ae2d9da6694adf338f96cf4de0982038763dc79
NguyenDucChi <nguyenducchi07102003@gmail.com>
2024-05-30 00:02:18 +0700

    Add model Discount

---
+++ JewelleryShop/JewelleryShop/Controllers/DiscountController.cs
@@ -0,0 +1,15 @@
+﻿using JewelleryShop.DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JewelleryShop.API.Controllers
+{
+    public class DiscountController : Controller
+    {
+        private readonly JewelleryDBContext _context;
+
+        public DiscountController(JewelleryDBContext context)
+        {
+            _context = context;
+        }
+    }
+}
