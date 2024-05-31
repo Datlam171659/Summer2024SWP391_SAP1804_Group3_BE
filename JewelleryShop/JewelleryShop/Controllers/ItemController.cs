2e6182e94997d48744aeb472025980ebe23e4f2d
NguyenDucChi <nguyenducchi07102003@gmail.com>
2024-06-01 01:16:41 +0700

    added soft deletion in ItemController ...

--- JewelleryShop/JewelleryShop/Controllers/ItemController.cs
+++ JewelleryShop/JewelleryShop/Controllers/ItemController.cs
@@ -105,5 +105,22 @@ namespace JewelleryShop.API.Controllers
 
             return NoContent();
         }
+
+        [HttpPut("softdelete/{id}")]
+        public async Task<IActionResult> SoftDeleteItem(string id)
+        {
+            var item = await _context.Items.FindAsync(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            item.Status = "out stock";
+            _context.Items.Update(item);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
