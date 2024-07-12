7bcd7f3273c37b7b2f1719cac6ca11a01aea2fa0
Hoàng Anh Đức <duchase171572@fpt.edu.vn>
2024-07-12 10:16:09 +0700

    Added: ExchangeRateService, GoldPriceService

---
+++ JewelleryShop/JewelleryShop.Business/Service/ExchangeRateService.cs
@@ -0,0 +1,57 @@
+﻿using JewelleryShop.Business.Service.Interface;
+using JewelleryShop.DataAccess.Models;
+using JewelleryShop.DataAccess.Models.ViewModel.ImgBBViewModel;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelleryShop.Business.Service
+{
+    public class ExchangeRateResponse
+    {
+        public string Base { get; set; }
+        public DateTime Date { get; set; }
+        public long TimeLastUpdated { get; set; }
+        public Dictionary<string, decimal> Rates { get; set; }
+    }
+
+    public class ExchangeRateService
+    {
+        private readonly HttpClient _httpClient;
+        private const string BaseUrl = "https://api.exchangerate-api.com/v4/latest/USD";
+
+        public ExchangeRateService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ExchangeRateResponse> GetLatestRatesAsync()
+        {
+            var response = await _httpClient.GetAsync(BaseUrl);
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<ExchangeRateResponse>(content);
+        }
+
+        public async Task<decimal?> GetRateForVndAsync()
+        {
+            var response = await _httpClient.GetAsync(BaseUrl);
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            var exchangeRateResponse = JsonConvert.DeserializeObject<ExchangeRateResponse>(content);
+
+            if (exchangeRateResponse.Rates.TryGetValue("VND", out var rate))
+            {
+                return rate;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ JewelleryShop/JewelleryShop.Business/Service/GoldPriceService.cs
@@ -0,0 +1,45 @@
+﻿using JewelleryShop.Business.Service.Interface;
+using JewelleryShop.DataAccess.Models;
+using JewelleryShop.DataAccess.Models.ViewModel.ImgBBViewModel;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelleryShop.Business.Service
+{
+    public class GoldPriceResponse
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string UpdatedAtReadable { get; set; }
+    }
+
+    public class GoldPriceService : IGoldPriceService
+    {
+        private readonly HttpClient _httpClient;
+        private const string BaseUrl = "https://api.gold-api.com/price/XAU";
+
+        public GoldPriceService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<decimal?> GetGoldPriceAsync()
+        {
+            var response = await _httpClient.GetAsync(BaseUrl);
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            var goldPriceResponse = JsonConvert.DeserializeObject<GoldPriceResponse>(content);
+            return goldPriceResponse.Price;
+        }
+    }
+}
